8be95d6552763c527e5bb59943a2647ca878eae3
Tomek Wiszniewski <tomek@wiszniewski.cc>
2018-11-01 15:23:45 +0100

    Form submission in static app

--- static/public/my-experience.js
+++ static/public/my-experience.js
@@ -1,32 +1,66 @@
-fetch('http://localhost:9977/index.html')
+const processResponse = html => {
+  const template = document.createElement('div');
+  template.innerHTML = html;
+
+  const links = [...template.querySelectorAll('link')];
+  const scripts = [...template.querySelectorAll('script')];
+  [...links, ...scripts, ...template.querySelectorAll('meta, title')].forEach(
+    element => element.parentNode.removeChild(element)
+  );
+
+  return {template, links, scripts};
+};
+
+fetch('http://localhost:9977')
   .then(response => response.text())
   .then(html => {
-    const template = document.createElement('div');
-    template.innerHTML = html;
-
-    [...template.querySelectorAll('link')].forEach(link =>
-      document.head.appendChild(link)
-    );
-
+    const {template, links, scripts} = processResponse(html);
+    links.forEach(link => document.head.appendChild(link));
     const clonedScripts = document.createDocumentFragment();
-    [...template.querySelectorAll('script')].forEach(script => {
-      script.parentNode.removeChild(script);
-      clonedScripts.appendChild(
-        Object.assign(document.createElement('script'), {
-          textContent: script.textContent,
-        })
-      );
+    scripts.forEach(script => {
+      const clonedScript = Object.assign(document.createElement('script'), {
+        textContent: script.textContent,
+      });
+      clonedScripts.appendChild(clonedScript);
     });
     document.head.appendChild(clonedScripts);
 
     customElements.define(
       'my-experience',
       class extends HTMLElement {
-        connectedCallback() {
-          [...template.childNodes].forEach(node => {
-            this.appendChild(node.cloneNode(true));
+        constructor() {
+          super();
+          this.template = template;
+
+          this.addEventListener('submit', async event => {
+            event.preventDefault();
+            const form = event.target;
+            const body = new URLSearchParams();
+            for (const [key, value] of new FormData(form)) {
+              body.append(key, value);
+            }
+            const response = await fetch(form.target, {
+              method: form.method,
+              mode: 'cors',
+              body,
+            });
+            const newHtml = await response.text();
+            this.template = processResponse(newHtml).template;
+            this.refreshDom();
           });
         }
+
+        connectedCallback() {
+          this.refreshDom();
+        }
+
+        refreshDom() {
+          [...this.childNodes].forEach(node => this.removeChild(node));
+          [...this.template.childNodes].forEach(node =>
+            this.appendChild(node.cloneNode(true))
+          );
+        }
+
       }
     );
   });
--- static/server.js
+++ static/server.js
@@ -1,20 +1,26 @@
 // server.js
 const path = require('path');
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const middleware = [express.static(path.join(__dirname, 'public'))];
+const router = express.Router();
+router.all('/', (request, response) =>
+  response.render('index', {
+    data: request.body || {},
+  })
+);
+
+const middleware = [
+  express.static(path.join(__dirname, 'public')),
+  bodyParser.urlencoded({extended: false}),
+  cors(),
+  router,
+];
 app.use(middleware);
 
-const router = express.Router();
-app.use('/', router);
-router.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.listen(9977, () => {
-  console.log(`App running at http://localhost:9977`);
-});
+app.listen(9977, () => console.log(`App running at http://localhost:9977`));
